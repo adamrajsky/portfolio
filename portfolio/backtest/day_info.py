8be4b5fbbfe155eef8bff3f5340e9bdf223d3996
Adam Rajsky <adamko.rajsky@gmail.com>
2023-08-20 18:49:37 +0200

    Add conversion to json serializable dict to day_info

--- portfolio/backtest/day_info.py
+++ portfolio/backtest/day_info.py
@@ -1,3 +1,5 @@
+import json
+
 class DayInfo:
     def __init__(self, date, current_bankroll, current_positions, daily_data, total_position_value, transactions_so_far, traded_lots_so_far):
         self.date = date
@@ -7,3 +9,15 @@ class DayInfo:
         self.total_position_value = total_position_value
         self.transactions_so_far = transactions_so_far
         self.traded_lots_so_far = traded_lots_so_far
+
+    def to_json_serializable_dict(self):
+        dict = {
+            'date' : str(self.date),
+            'current_bankroll' : self.current_bankroll,
+            'current_positions' : self.current_positions.to_dict(),
+            'daily_data' : self.daily_data.to_dict(),
+            'total_position_value' : self.total_position_value,
+            'transactions_so_far' : self.transactions_so_far,
+            'traded_lots_so_far' : self.traded_lots_so_far
+        }
+        return dict
